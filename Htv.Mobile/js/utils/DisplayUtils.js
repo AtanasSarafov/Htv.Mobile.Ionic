7fd271d1704ecb2eb43256c0ade0b48b5c21a950
Krasimir Fartsov <kfartsov@gmail.com>
2017-06-09 16:22:03 +0300

    Video List page done and all text translated in bulgarian

--- Htv.Mobile/js/utils/DisplayUtils.js
+++ Htv.Mobile/js/utils/DisplayUtils.js
@@ -1,4 +1,4 @@
-angular.module('App').service('displayUtils', function ($translate) {
+﻿angular.module('App').service('displayUtils', function ($translate) {
 
     this.getDisplayDate = function (dateString) {
         var date = new Date(dateString);
@@ -14,7 +14,81 @@ angular.module('App').service('displayUtils', function ($translate) {
             $translate.instant('September'),
             $translate.instant('October'),
             $translate.instant('November'),
-            $translate.instant('December')
+            $translate.instant('December'),
+            $translate.instant('ContactForm'),
+            $translate.instant('OtherContacts'),
+            $translate.instant('Send'),
+            $translate.instant('Internet'),
+            $translate.instant('Phone'),
+            $translate.instant('Adress'),
+            $translate.instant('News'),
+            $translate.instant('Videos'),
+            $translate.instant('Support'),
+            $translate.instant('Donation'),
+            $translate.instant('More'),
+            $translate.instant('Contacts'),
+            $translate.instant('Related'),
+            $translate.instant('AllInOne'),
+            $translate.instant('Qualities'),
+            $translate.instant('LowQuality'),
+            $translate.instant('HighQuality'),
+            $translate.instant('SdAvailable'),
+            $translate.instant('HdAvailable'),
+            $translate.instant('FullVersion'),
+            $translate.instant('AdressDetails'),
+            $translate.instant('Other1'),
+            $translate.instant('Other2'),
+            $translate.instant('Company'),
+            $translate.instant('Street'),
+            $translate.instant('City'),
+            $translate.instant('DonationIntro'),
+            $translate.instant('DonationTitle'),
+            $translate.instant('PayPal'),
+            $translate.instant('BankTransfer'),
+            $translate.instant('PayPalTxt'),
+            $translate.instant('BankTransferTxt'),
+            $translate.instant('BGN'),
+            $translate.instant('EUR'),
+            $translate.instant('USD'),
+            $translate.instant('DSK'),
+            $translate.instant('FIBANK'),
+            $translate.instant('EUROBANK'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         ];
--- Htv.Mobile/views/LiveTv/insertDate.js
+++ Htv.Mobile/views/LiveTv/insertDate.js
@@ -1,9 +1,9 @@
-function insertDate() {
+﻿function insertDate() {
 
     var n = new Date();
     var y = n.getFullYear();
     var m = n.getMonth();
     var d = n.getDate();
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    document.getElementById("schedule-date").innerHTML = "Schedule for " + d + " " + months[m] + " " + y;
+    var months = ["Януари", "Февруари", "Март", "Април", "Май", "Юни", "Юли", "Август", "SeСептемвриp", "Октомври", "Ноември", "Декември"];
+    document.getElementById("schedule-date").innerHTML = "Програма за " + d + " " + months[m] + " " + y;
 }
